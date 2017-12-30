b9990fc2c9370155a2b5fbd4a2b6c5e62cc1ca51
Ominousness <ominousness@protonmail.com>
2017-12-30 11:50:53 +0100

    fixed metadata

--- mix.exs
+++ mix.exs
@@ -12,11 +12,12 @@ defmodule Crypt3.Mixfile do
     [
       app: :crypt3,
       compilers: [:nif] ++ Mix.compilers,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
       package: package(),
+      description: description(),
       source_url: "https://github.com/ominousness/crypt3"
     ]
   end
@@ -31,9 +32,14 @@ defmodule Crypt3.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
+  end
+
+  defp description do
+    "crypt(3) NIF for Elixir."
   end
 
   defp package do
